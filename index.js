d42c042c92e2c7641e6b05deec41d76d03369ba4
acsolabs <sofian.achbabi@gmail.com>
2026-03-24 16:32:53 +0100

    feat : class Question

---
+++ index.js
@@ -0,0 +1,36 @@
+const questionBalise = document.getElementById("question");
+
+class Question {
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
+
+  isCorrectAnswer = function (choice) {
+    return choice === this.answer;
+  };
+}
+
+const questions = [
+  new Question(
+    "Quelle méthode Javascript permet de filtrer les éléments d'un tableau",
+    ["indexOf()", "map()", "filter()", "reduce()"],
+    "filter()",
+  ),
+  new Question(
+    "Quelle méthode Javascript permet de vérifier si un élément figure dans un tableau",
+    ["isNaN()", "includes()", "findIndex()", "isOdd()"],
+    "includes()",
+  ),
+  new Question(
+    "Quelle méthode transforme du JSON en un objet Javascript ?",
+    ["JSON.parse()", "JSON.stringify()", "JSON.object()", "JSON.toJS"],
+    "JSON.parse()",
+  ),
+  new Question(
+    "Quel objet Javascript permet d'arrondir à l'entier le plus proche",
+    ["Math.ceil()", "Math.floor()", "Math.round()", "Math.random()"],
+    "Math.round()",
+  ),
+];
